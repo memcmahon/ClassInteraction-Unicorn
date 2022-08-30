1a8973c8f3b0e608349770ce3fe4cbdc02a8c0a3
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2022-08-30 11:38:04 -0600

    Add project files.

---
+++ Unicorn/Power.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unicorn
+{
+    public class Power
+    {
+        public string Name;
+        public int EnergyRequired;
+
+        public Power(string name, int energyRequired)
+        {
+            Name = name;
+            EnergyRequired = energyRequired;
+        }
+    }
+}
---
+++ Unicorn/Unicorn.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unicorn
+{
+    public class Unicorn
+    {
+        public string Name;
+        public string Color { get; set; }
+        public List<Power> MagicalPowers;
+        public int Energy;
+
+        public Unicorn(string name, string color)
+        {
+            Name = name;
+            Color = color;
+            MagicalPowers = new List<Power>();
+            Energy = 100;
+        }
+
+        public void AddPower(Power magicalPower)
+        {
+            MagicalPowers.Add(magicalPower);
+        }
+
+        public void DisplayPowers()
+        {
+            foreach (var power in MagicalPowers)
+            {
+                Console.WriteLine($"*-*-*{power.Name}*.*.*");
+            }
+        }
+
+        public void Perform(Power power)
+        {
+            if (MagicalPowers.Contains(power))
+                Energy -= power.EnergyRequired;
+        }
+
+        public void Rest(int time)
+        {
+            Energy += time;
+        }
+    }
+}
